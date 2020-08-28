1b665515381f009e0ea56c46cfd1a02c8c2a5634
TARISO <toni.ariso@gmail.com>
2020-08-28 09:49:43 +0200

    add js

---
+++ main.js
@@ -0,0 +1,88 @@
+new Vue({
+  el: '#app',
+  data: {
+    currentQuote: '',
+    currentQuoteAuthor: '',
+    quotes_api_url:
+      'http://api.forismatic.com/api/1.0/?method=getQuote&lang=en&format=json',
+    quotes_proxy_url: 'https://cryptic-thicket-75396.herokuapp.com/',
+  },
+  methods: {
+    newQuote: function () {
+      fetch(this.quotes_proxy_url + this.quotes_api_url)
+        .then((response) => response.json())
+        .then((quotes) => {
+          this.currentQuote = quotes.quoteText;
+          this.currentQuoteAuthor = quotes.quoteAuthor;
+        });
+    },
+  },
+});
+
+// const quoteContainer = document.getElementById('quote-container');
+// const twitterBtn = document.getElementById('twitter');
+
+// const loader = document.getElementById('loader');
+
+// // Show Loading
+// function showLoadingSpinner() {
+//   loader.hidden = false;
+//   quoteContainer.hidden = true;
+// }
+
+// // Hide Loading
+// function removeLoadingSpinner() {
+//   if (!loader.hidden) {
+//     quoteContainer.hidden = false;
+//     loader.hidden = true;
+//   }
+// }
+
+// // Get Quote From API
+// async function getQuote() {
+//   showLoadingSpinner();
+//   const proxyUrl = 'https://cryptic-thicket-75396.herokuapp.com/';
+//   const apiUrl =
+//     'http://api.forismatic.com/api/1.0/?method=getQuote&lang=en&format=json';
+//   try {
+//     const response = await fetch(proxyUrl + apiUrl);
+//     const data = await response.json();
+
+//     // If Author is blank, add 'Unknown'
+//     if (data.quoteAuthor === '') {
+//       authorText.innerText = 'Unknown';
+//     } else {
+//       authorText.innerText = data.quoteAuthor;
+//     }
+
+//     // Reduce font size for long quotes
+//     if (data.quoteText.length > 120) {
+//       quoteText.classList.add('long-quote');
+//     } else {
+//       quoteText.classList.remove('long-quote');
+//     }
+
+//     quoteText.innerText = data.quoteText;
+
+//     // Stop Loader, Show Quote
+//     removeLoadingSpinner();
+
+//     // throw new Error('oops')
+//   } catch (error) {
+//     // Possible infite loop due recursive calling function getQuote
+//     // console.log(error);
+//     getQuote();
+//   }
+// }
+
+// // Tweet Quote
+// function tweetQuote() {
+//   const quote = quoteText.innerText;
+//   const author = authorText.innerText;
+//   const twitterUrl = `https://twitter.com/intent/tweet?text=${quote} - ${author}`;
+//   window.open(twitterUrl, '_blank');
+// }
+
+// // Event Listeners
+// newQuoteBtn.addEventListener('click', getQuote);
+// twitterBtn.addEventListener('click', tweetQuote);
